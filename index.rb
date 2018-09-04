771e55bf1b04902dfb088f236cacfc27a6e22880
afeiship <1290657123@qq.com>
2018-09-04 21:38:28 +0800

    issue: basic

--- index.rb
+++ index.rb
@@ -3,6 +3,8 @@ require 'uri'
 require 'cgi'
 require 'json'
 require 'base64'
+require "net/http"
+
 
 # url login:
 url_prelogin = 'https://login.sina.com.cn/sso/prelogin.php'
@@ -31,17 +33,42 @@ res = open( "#{url_prelogin}?#{URI.encode_www_form(prelogin_data)}" )
 prelogin_response = JSON.parse PRELOGIN_JSONP_RE.match(res.read)[0];
 
 
-# puts prelogin_response.class
-# puts prelogin_response.match(/"\{(.*)\}"/)
-
-
 pwdkey = prelogin_response['servertime'].to_s + "\t" + prelogin_response['nonce'].to_s + "\n" + password.to_s
 pub = OpenSSL::PKey::RSA::new
 pub.e = 65537
 pub.n = OpenSSL::BN.new(prelogin_response['pubkey'],16)
 sp = pub.public_encrypt(pwdkey).unpack('H*').first
 
-p sp
+data = {
+  'entry'=> 'weibo',
+  'gateway'=>1,
+  'from'=> '',
+  'savestate'=> 30,
+  'useticket'=> 0,
+  'pagerefer'=> '',
+  'vsnf'=> 1,
+  'service'=> 'sso',
+  'pwencode'=> 'rsa2',
+  'sr'=> '1920 * 1200',
+  'encoding'=> 'UTF-8',
+  'cdult'=> 3,
+  'domain'=> 'sina.com.cn',
+  'returntype'=> 'TEXT'
+};
+
+data.merge!(prelogin_response).merge!({
+    'su'=> su,
+    'sp'=> sp,
+    'prelt'=> 56
+})
+
+login_response = Net::HTTP.post_form(URI(url_login),data)
+
+url_ticket = JSON.parse(login_response.body)['crossDomainUrlList'][0]
+
+weibo_sso_res = open(url_ticket);
+
+p weibo_sso_res.meta['set-cookie']
 
 
 # puts URI::encode('1290657123@qq.com')
